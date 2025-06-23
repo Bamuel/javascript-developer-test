5e269902a3f69b7280724ffd318a5dab3096b2fd
Samuel Levin <me@bamuel.com>
2025-06-23 15:24:43 +1000

    Implement getArnieQuotes function to fetch quotes from provided URLs

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,10 +1,28 @@
-const { httpGet } = require('./mock-http-interface');
+const {httpGet} = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+
+    // Create an array of promises for each URL
+    const promises = urls.map(async (url) => {
+        try {
+            const response = await httpGet(url);
+
+            if (response.status === 200) {
+                const body = JSON.parse(response.body);
+                return {'Arnie Quote': body.message};
+            } else {
+                const body = JSON.parse(response.body);
+                return {'FAILURE': body.message};
+            }
+        } catch (error) {
+            return {'FAILURE': error.message};
+        }
+    });
+
+    // Wait for all promises to resolve and return the results
+    return Promise.all(promises);
 };
 
 module.exports = {
-  getArnieQuotes,
+    getArnieQuotes,
 };
